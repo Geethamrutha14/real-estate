1c96ce5deecd5e20d47a04b0290fb643371febe3
Geethamrutha14 <geethamruthapalla4@email.com>
2025-06-21 23:56:14 +0530

    completed stylind header

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import {FaSearch} from 'react-icons/fa';
+import {Link} from 'react-router-dom';
 export default function Header() {
   return (
     <div>
@@ -7,17 +8,35 @@ export default function Header() {
       <header className='bg-slate-200 shadow-md'>
 
        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
          <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
         <span className='text-slate-500' >Real</span>
         <span className='text-slate-700'>Estate</span>
-
       </h1>
-      <form className='bg-slate-100 rounded-lg'>
-        <input type="text" placeholder='Search...' className='rounded-md bg-slate-100 outline-none p-2'/>
+
+      </Link>
+      <form className='bg-slate-100 rounded-lg p-3 flex items-center justify-center'>
+        <input type="text" placeholder='Search...' className='w-24 sm:w-64 bg-transparent focus:outline-none'/>
+        <FaSearch className='text-slate-600'/>
       </form>
+
+      <ul className='flex gap-4'>
+
+        <Link to='/'>
+        <li className='text-slate-700 hover:underline hidden sm:inline'>HOME</li>
+        </Link>
+
+        <Link to='about'>
+        <li className='text-slate-700 hover:underline hidden sm:inline'>ABOUT</li>
+        </Link>
+
+        <Link to='sign-in'>
+        <li className='text-slate-700 hover:underline hidden sm:inline'>SIGN-IN</li>
+        </Link>
+       </ul>
        </div>
 
-
+       
       </header>
     </div>
   )
