39c27b980b8cb29e2d92148310eaaeb20b3aca5d
Geethamrutha14 <geethamruthapalla4@email.com>
2025-06-22 13:15:06 +0530

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO)
+.then(()=>{
+    console.log("connected...");
+    // console.log('Mongo URI:', process.env.MONGO);
+} )
+.catch((err)=>{
+    console.log(err);
+});
+
 
 const app = express();
 
