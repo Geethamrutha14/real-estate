985c71e2dc32f9df1031f41e08013ad6c9e67b6d
Geethamrutha14 <geethamruthapalla4@email.com>
2025-07-21 20:24:35 +0530

    updated the UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,10 +1,14 @@
 import { useSelector } from 'react-redux'
+import {useRef} from 'react'
+
 export default function Profile() {
   const {currentUser} = useSelector((state) => state.user);
+  const fileRef = useRef(null);
   return (
      <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Profile</h1>
-      <img src={currentUser.avatar} alt="profile" className='rounded-full self-center h-24 w-24 object-cover cursor-pointer mt-2'/>
+      <input type="file" ref={fileRef} hidden accept='image/*'/>
+      <img src={currentUser.avatar} onClick={()=> fileRef.current.click()} alt="profile" className='rounded-full self-center h-24 w-24 object-cover cursor-pointer mt-2'/>
       <form className='flex flex-col gap-4 '>
         <input type="text" placeholder='username' id='username'
          className='border p-3 rounded-lg'/>
